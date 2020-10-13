061226d5e5b28a8a13cfe1a0feb619470ffb604c
Michał Hajok <hajokmichal@gmail.com>
2020-10-13 17:03:21 +0200

    a

--- models/Auth.js
+++ models/Auth.js
@@ -1,41 +1,42 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const AuthSchema = mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    lastname: {
-        type: String,
-        required: true
-    },
-    sex: {
-        type: String,
-        required: true
-    },
-    birthday: {
-        type: Date
-    },
-    phone: {
-        type: String,
-        required: true
-    },
-    city: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-})
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  sex: {
+    type: String,
+    required: true,
+  },
+  birthday: {
+    type: Date,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('Auth', AuthSchema)
+module.exports = mongoose.model("Auth", AuthSchema);
